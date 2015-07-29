0cded7b4f777f147cf2e3d9f0cbde531ffe6180b
Thomas Sutton <me@thomas-sutton.id.au>
2015-07-29 15:25:56 +1000

    Let's benchmark fib

---
+++ benchmark/criterion.hs
@@ -0,0 +1,19 @@
+module Main (main) where
+
+import           Criterion.Main
+
+fib m | m < 0     = error "Negative"
+      | otherwise = go m
+  where
+    go 0 = 0
+    go 1 = 1
+    go n = go (n-1) + go (n-2)
+
+main :: IO ()
+main = defaultMain
+       [ bgroup "fib" [ bench "1" $ whnf fib 1
+                      , bench "5" $ whnf fib 5
+                      , bench "9" $ whnf fib 9
+                      , bench "11" $ whnf fib 11
+                      ]
+       ]
